173a38317bc888bbfe2e626b6b050d19ad653ec8
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-02 13:02:52 +0300

    z_function .gitignore and header repair.

--- strings/z_function/z_function.h
+++ strings/z_function/z_function.h
@@ -3,8 +3,6 @@
 #include <string>
 #include <vector>
 
-#include <iostream>
 
-
-void build_z_function(const std::string& string);
+void build_z_function(std::vector<int>& z_function, const std::string& string);
 std::vector<int> find(const std::string& str_where_to_find, const std::string& str_to_find);
